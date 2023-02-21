2c638416d721e27b50507e0afd49e5b80f37da07
Allassane <dickoallassane1997@gmail.com>
2023-02-21 15:39:40 +0100

    Update schema to change the 'add species column' feature

--- schema.sql
+++ schema.sql
@@ -1,12 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-create table animals(   
+create table animalsTEST(   
 id int generated always as identity,
 name varchar NOT NULL,
 date_of_birth date NOT NULL,
 escape_attempts int,
 neutered boolean, 
 weight_kg decimal,
-species varchar,
 PRIMARY KEY (id)
 );
+
+ALTER TABLE animalsTEST ADD species varchar;
