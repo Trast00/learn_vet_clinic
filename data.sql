e97867ad6ecbfe8e2812c9601e5b0863a1156021
Allassane <dickoallassane1997@gmail.com>
2023-02-22 14:25:58 +0100

    Update queries.sql to add all queries

--- data.sql
+++ data.sql
@@ -1,5 +1,15 @@
 /* */
 
+insert into animals 
+values (DEFAULT, 'Agumon', '2020-02-03', 0, true, 10.23);
+insert into animals 
+values (DEFAULT, 'Gabumon', '2018-10-15', 2, true, 8);
+insert into animals 
+values (DEFAULT, 'Pikachu', '2021-01-07', 0, false, 15.04);
+insert into animals 
+values (DEFAULT, 'Devimon', '2017-05-12', 0, true, 11);
+
+
 /* Populate database with sample data. */
 
 insert into animals 
@@ -20,22 +30,23 @@ values (DEFAULT, 'Ditto', '2022-05-14', 4, true, 22);
 
 /* Project part 3 */
 /* owner */
-insert into owners (DEFAULT, 'Sam Smith', 34)
-insert into owners (DEFAULT, 'Jennifer Orwell', 19)
-insert into owners (DEFAULT, 'Bob', 45)
-insert into owners (DEFAULT, 'Melody Pon', 77)
-insert into owners (DEFAULT, 'Dean Winchester', 14)
-insert into owners (DEFAULT, 'Jodie Whittaker', 38)
+insert into owners values (DEFAULT, 'Sam Smith', 34);
+insert into owners values (DEFAULT, 'Jennifer Orwell', 19);
+insert into owners values (DEFAULT, 'Bob', 45);
+insert into owners values (DEFAULT, 'Melody Pon', 77);
+insert into owners values (DEFAULT, 'Dean Winchester', 14);
+insert into owners values (DEFAULT, 'Jodie Whittaker', 38);
 
 /* species */
-insert into species (DEFAULT, 'Pokemon')
-insert into species (DEFAULT, 'Digimon')
-
+insert into species values (DEFAULT, 'Pokemon');
+insert into species values (DEFAULT, 'Digimon');
 
 
 UPDATE animals SET species_id = 2 where name LIKE '%mon';
-UPDATE animals SET species_id = 1 where name != 'Digimon';
+UPDATE animals SET species_id = 1 where species_id is NULL;
 
 UPDATE animals SET owner_id = 1 where name = 'Agumon';
 UPDATE animals SET owner_id = 2 where name = 'Gabumon' OR name = 'Pikachu';
-UPDATE animals SET owner_id = 1 where name = 'Agumon';
+UPDATE animals SET owner_id = 3 where name = 'Plantmon';
+UPDATE animals SET owner_id = 4 where name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE animals SET owner_id = 5 where name = 'Angemon' or name = 'Boarmon';
--- queries.sql
+++ queries.sql
@@ -51,3 +51,36 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
 
 
+/* Project part 3 */
+/* What animals belong to Melody Pond? */
+SELECT * from animals join owners on animals.owner_id = owners.id
+where full_name = 'Melody Pond';
+
+/* List of all animals that are pokemon (their type is Pokemon). */
+SELECT * from animals join species on animals.species_id = species.id
+where species.name = 'Pokemon';
+
+/* List all owners and their animals, remember to include those that don't own any animal. */
+SELECT * from owners left join animals on owners.id = animals.owner_id;
+
+/* How many animals are there per species? */
+SELECT species.name, COUNT(*) from species 
+left join animals on species.id = animals.species_id
+group by species.name;
+
+/* List all Digimon owned by Jennifer Orwell */
+SELECT * from owners 
+join animals on owners.id = animals.owner_id
+join species on species.id = animals.species_id
+where owners.full_name = 'Jennifer Orwell';
+
+/* List all animals owned by Dean Winchester that haven't tried to escape. */
+SELECT * from owners 
+join animals on owners.id = animals.owner_id
+where owners.full_name = 'Dean Winchester'
+AND animals.escape_attempts = 0;
+
+/* Who own the most animals ? answer: Melody Pond with 3 animals */
+SELECT owners.full_name, COUNT(*) from owners 
+left join animals on owners.id = animals.owner_id
+group by owners.full_name;
