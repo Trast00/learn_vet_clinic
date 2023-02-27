5fcdbc2fd21beec12f22f642204d802127cb55a7
Allassane <dickoallassane1997@gmail.com>
2023-02-27 15:25:03 +0100

    Add insertion to dabase

--- queries.sql
+++ queries.sql
@@ -153,3 +153,9 @@ left join animals on visits.animal_id = animals.id
 left join species on animals.species_id = species.id
 where vets.name = 'Maisy Smith'
 group by vets.name, species.id;
+
+
+/* Project part 5: increase speed  */
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
