0a3dd558ddb6cbc05a68a2d74f160c15c35d4df8
Allassane <dickoallassane1997@gmail.com>
2023-02-27 16:16:11 +0100

    Add indexs for animals_id column of visits

--- queries.sql
+++ queries.sql
@@ -156,6 +156,6 @@ group by vets.name, species.id;
 
 
 /* Project part 5: increase speed  */
-SELECT COUNT(*) FROM visits where animal_id = 4;
-SELECT * FROM visits where vet_id = 2;
-SELECT * FROM owners where email = 'owner_18327@mail.com';
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
--- schema.sql
+++ schema.sql
@@ -66,4 +66,5 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id F
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
-
+/* increase speed */
+CREATE INDEX animal_id_index ON visits(animal_id ASC);
